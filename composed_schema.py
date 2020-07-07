75e21bbecbeea1d8a154b7542d49c60cad6856d6
Justin Black <justin.a.black@gmail.com>
2020-07-07 14:33:55 -0700

    Refactors make_dynamic_class, simplifies testing, adds enum subclass example

--- composed_schema.py
+++ composed_schema.py
@@ -1,49 +1,44 @@
 import pdb
+import datetime
 from enum import Enum
 
-class Cat:
-    def __init__(self, *args, **kwargs):
-        self.color = kwargs.get('color')
+
+class NoneEnum(Enum):
+    NONE = None
+
+class TrueEnum(Enum):
+    TRUE = True
+
+class FalseEnum(Enum):
+    FALSE = False
 
 
-def make_dynamic_class(bases):
+def make_dynamic_class(*bases):
     """
     Returns a new DynamicBaseClasses class that is made with the subclasses bases
     TODO: lru_cache this
 
     Args:
-        bases (tuple): the base classes that DynamicBaseClasses inherits from
-        cls_dict (dict): the class dictionary
+        bases (list): the base classes that DynamicBaseClasses inherits from
     """
+    if issubclass(bases[-1], Enum):
+        # enum based classes
+        bases = list(bases)
+        source_enum = bases.pop()
+        assert issubclass(source_enum, Enum), "The last entry in bases must be a subclass of Enum"
+        source_enum_bases = source_enum.__bases__
+        assert (source_enum_bases[-1] is Enum), "The last entry in source_enum_bases must be Enum"
+        bases.extend(source_enum_bases)
+        # DynamicBaseClassesEnum cannot be used as a base class
+        # TODO: also add any bases in source_enum to DynamicBaseClassesEnum
+        class DynamicBaseClassesEnum(*bases):
+            for choice in source_enum:
+                # source_enum cannot be used as a base class, so copy its enum values into our new enum
+                locals()[choice.name] = choice.value
+        return DynamicBaseClassesEnum
+    # object based classes
     new_cls = type('DynamicBaseClasses', bases, {})
     return new_cls
-
-def make_dynamic_enum(bases, value):
-    """
-    TODO: lru_cache this
-    Make a dynamic enum to store the values True, False, and None
-    because type(None), and bool may not be used as base classes
-    and they may not be used as superclasses of Enum
-
-    Args:
-        bases (tuple): the base classes that DynamicBaseClasses inherits from
-        value (None/bool): the value that we set as an enum
-    """
-    # this cannot be used as a base class
-    class DynamicBaseClassesEnum(*bases, Enum):
-        # TODO loop through the input enum and set it here with locals
-        if value is None:
-            NONE = None
-        elif value is True:
-            TRUE = True
-        elif value is False:
-            FALSE = False
-        else:
-            raise ValueError('Invalid value passed in')
-    return DynamicBaseClassesEnum
-
-
-import datetime
 
 
 class IntModel(int):
@@ -54,6 +49,10 @@ inheritable_primitive_types = (int, float, str, datetime.date, datetime.datetime
 
 class StringEnum(str, Enum):
     RED = 'red'
+
+class Cat:
+    def __init__(self, *args, **kwargs):
+        self.color = kwargs.get('color')
 
 
 def get_new_class(cls, *args, **kwargs):
@@ -67,28 +66,24 @@ def get_new_class(cls, *args, **kwargs):
     """
     if args and not kwargs and len(args) == 1:
         arg = args[0]
-        is_none = args[0] is None
-        is_bool = isinstance(arg, bool)
-        if is_none or is_bool:
+        if arg is None:
             # type(None) and bool cannot be subclassed so we must use Enums
             # to store None, True, and False
-            return make_dynamic_enum(
-                (cls, ), arg
-            )
+            return make_dynamic_class(cls, NoneEnum)
+        elif arg is True:
+            return make_dynamic_class(cls, TrueEnum)
+        elif arg is False:
+            return make_dynamic_class(cls, FalseEnum)
         elif isinstance(arg, int):
-            return make_dynamic_class((cls, IntModel))
+            return make_dynamic_class(cls, IntModel)
         elif isinstance(arg, float):
-            return make_dynamic_class((cls, float))
+            return make_dynamic_class(cls, float)
         elif isinstance(arg, str):
-            chosen_cls = StringEnum
-            if issubclass(chosen_cls, StringEnum):
-                # TODO get enum values
-                pass
-            return make_dynamic_class((cls, StringEnum))
+            return make_dynamic_class(cls, StringEnum)
         else:
             raise ValueError('case not handled yet')
     elif kwargs:
-        return make_dynamic_class((cls, Cat))
+        return make_dynamic_class(cls, Cat)
     raise ValueError('Arguments are required to make a new class')
 
 
@@ -105,7 +100,12 @@ class ComposedSchema:
         if issubclass(cls, Enum):
             # we are creating new instances of DynamicBaseClassesEnum, Enum based class
             # this sets enum values when we create the DynamicBaseClassesEnum
-            inst = object.__new__(cls)
+            new_instance_class = object
+            for base_cls in cls.__bases__:
+                if base_cls in inheritable_primitive_types:
+                    new_instance_class = base_cls
+                    break
+            inst = new_instance_class.__new__(cls)
             inst._value_ = args[0]
             return inst
         elif issubclass(cls, inheritable_primitive_types):
@@ -143,30 +143,28 @@ for value in {None, True, False}:
 # IntModel
 value = 5
 a = ComposedSchema(value)
-for cls in [ComposedSchema, IntModel, int]:
-    assert issubclass(a.__class__, cls)
-    assert isinstance(a, cls)
+bases = (ComposedSchema, IntModel)
+assert a.__class__.__bases__ == bases
+# Note: for all bases isinstance and issubclass returns True when checking a and a.__class__ against them
 assert a.value == value
 assert a == value
 
 # float
 value = 3.14
 a = ComposedSchema(value)
-for cls in [ComposedSchema, float]:
-    assert issubclass(a.__class__, cls)
-    assert isinstance(a, cls)
+bases = (ComposedSchema, float)
+assert a.__class__.__bases__ == bases
 assert a.value == value
 assert a == value
 
+# Enum model
+value = 'red'
+a = ComposedSchema(value)
+bases = (ComposedSchema, str, Enum)
+assert a.__class__.__bases__ == bases
+assert a.value == value
 
-# TODO subclass an enum
-# value = 'red'
-# a = ComposedSchema(value)
-# for cls in [ComposedSchema, str, Enum]:
-#     assert issubclass(a.__class__, cls)
-#     assert isinstance(a, cls)
-# assert a.value == value
-
+# composed schema that includes an eum
 
 # TODO get working for for composed class inside a composed class, where the innermost holds None
 # if
