5a829e59e46cdcafe579d8de6f2a721e1d3c98fd
Justin Black <justin.a.black@gmail.com>
2020-07-08 11:50:58 -0700

    Refactoring

--- composed_schema.py
+++ composed_schema.py
@@ -45,12 +45,14 @@ class IntModel(int):
     def __init__(self, *args, **kwargs):
         self.path = []
 
+
 inheritable_primitive_types = (int, float, str, datetime.date, datetime.datetime)
 
 class StringEnum(str, Enum):
     RED = 'red'
 
-class Cat:
+
+class Panther:
     def __init__(self, *args, **kwargs):
         self.color = kwargs.get('color')
 
@@ -58,41 +60,38 @@ class ListModel(list):
     pass
 
 
-def get_new_class(cls, *args, **kwargs):
-    """
-    For now we return dynamic classes of different bases depending upon the inputs
-    In real life this function will use the class discriminator and composed schema info to determine
-    the base classes that we will use to build new_cls
-
-    Returns:
-        new_cls (type): the new dynamic class that we have built
-    """
-    if args and not kwargs and len(args) == 1:
-        arg = args[0]
-        if arg is None:
-            # type(None) and bool cannot be subclassed so we must use Enums
-            # to store None, True, and False
-            return make_dynamic_class(cls, NoneEnum)
-        elif arg is True:
-            return make_dynamic_class(cls, TrueEnum)
-        elif arg is False:
-            return make_dynamic_class(cls, FalseEnum)
-        elif isinstance(arg, int):
-            return make_dynamic_class(cls, IntModel)
-        elif isinstance(arg, float):
-            return make_dynamic_class(cls, float)
-        elif isinstance(arg, str):
-            return make_dynamic_class(cls, StringEnum)
-        elif isinstance(arg, list):
-            if not arg:
-                return make_dynamic_class(cls, list)
-            return make_dynamic_class(cls, ListModel)
-        else:
-            raise ValueError('case not handled yet')
-    elif kwargs:
-        return make_dynamic_class(cls, Cat)
-    raise ValueError('Arguments are required to make a new class')
-
+def get_new_instance(self_class, cls, super_instance, *args, **kwargs):
+    if cls == self_class:
+        # we are making an instance of self, but instead of making self
+        # make a new class, new_cls
+        # which includes dynamic bases including self
+        # return an instance of that new class
+        new_cls = self_class._get_new_class(*args, **kwargs)
+        new_inst = new_cls.__new__(new_cls, *args, **kwargs)
+        return new_inst
+    if issubclass(cls, Enum):
+        # we are creating new instances of DynamicBaseClassesEnum, Enum based class
+        # this sets enum values when we create the DynamicBaseClassesEnum
+        new_instance_class = object
+        for base_cls in cls.__bases__:
+            if base_cls in inheritable_primitive_types:
+                new_instance_class = base_cls
+                break
+        inst = new_instance_class.__new__(cls)
+        inst._value_ = args[0]
+        return inst
+    elif issubclass(cls, inheritable_primitive_types):
+        # we are creating new instances of inheritable_primitive_types
+        inst = super_instance.__new__(cls, args[0])
+        inst.value = args[0]
+        return inst
+    # we are creating new instances of DynamicBaseClasses, list based class
+    if issubclass(cls, list):
+        inst = super_instance.__new__(cls)
+        inst.value = args[0]
+        return inst
+    # we are creating new instances of DynamicBaseClasses, object based class
+    return super_instance.__new__(cls)
 
 class ComposedSchema:
     @classmethod
@@ -100,11 +99,18 @@ class ComposedSchema:
         # we should only run validation once
         # so I will need to extract type checking, allowed value checking and validation checking into a class method
         # when classes are picked (with a discriminator or in a composed schema)
-        # they will be picked from inside a __new__ invocation so we need to move that validation checking into a class
+        # they will be picked from inside a __new__ invoPantherion so we need to move that validation checking into a class
         # method
         # in __init__ _validate will not be called because it was called prior in __new__
-        # when properties are changed like adding an item to an array or changing cat.color, _validate will be called
+        # when properties are changed like adding an item to an array or changing Panther.color, _validate will be called
         # __init__ will only be assignment
+        # we have a problem where a payload could move though multiple classes twice and create
+        # a cycle
+        # that could be solved by storing the set of classes that have been validated
+        # or by storing {
+        # md5_of_payload_being_deserialized: set(deserialized classes)
+        # }
+        # then we say for this given payload, I already know that I validated it
         if args and len(args) != 1:
             raise ValueError('When validating a value only, that single value must be passed in')
         if args and kwargs:
@@ -119,45 +125,56 @@ class ComposedSchema:
         pass
 
     def __new__(cls, *args, **kwargs):
-        if cls == ComposedSchema:
-            # we are making an instance of self, but instead of making self
-            # make a new class, new_cls
-            # which includes dynamic bases including self
-            # return an instance of that new class
-            new_cls = get_new_class(cls, *args, **kwargs)
-            new_inst = new_cls.__new__(new_cls, *args, **kwargs)
-            return new_inst
-        if issubclass(cls, Enum):
-            # we are creating new instances of DynamicBaseClassesEnum, Enum based class
-            # this sets enum values when we create the DynamicBaseClassesEnum
-            new_instance_class = object
-            for base_cls in cls.__bases__:
-                if base_cls in inheritable_primitive_types:
-                    new_instance_class = base_cls
-                    break
-            inst = new_instance_class.__new__(cls)
-            inst._value_ = args[0]
-            return inst
-        elif issubclass(cls, inheritable_primitive_types):
-            # we are creating new instances of inheritable_primitive_types
-            return super().__new__(cls, args[0])
-        # we are creating new instances of DynamicBaseClasses, object based class
-        return super().__new__(cls)
+        return get_new_instance(ComposedSchema, cls, super(), *args, **kwargs)
 
-    _nullable = True
+    @classmethod
+    def _get_new_class(cls, *args, **kwargs):
+        """
+        For now we return dynamic classes of different bases depending upon the inputs
+        In real life this function will use the class discriminator and composed schema info to determine
+        the base classes that we will use to build new_cls
+
+        Returns:
+            new_cls (type): the new dynamic class that we have built
+        """
+        if args and not kwargs and len(args) == 1:
+            arg = args[0]
+            if arg is None:
+                # type(None) and bool cannot be subclassed so we must use Enums
+                # to store None, True, and False
+                return make_dynamic_class(cls, NoneEnum)
+            elif arg is True:
+                return make_dynamic_class(cls, TrueEnum)
+            elif arg is False:
+                return make_dynamic_class(cls, FalseEnum)
+            elif isinstance(arg, int):
+                return make_dynamic_class(cls, IntModel)
+            elif isinstance(arg, float):
+                return make_dynamic_class(cls, float)
+            elif isinstance(arg, str):
+                return make_dynamic_class(cls, StringEnum)
+            elif isinstance(arg, list):
+                if not arg:
+                    return make_dynamic_class(cls, list)
+                return make_dynamic_class(cls, ListModel)
+            else:
+                raise ValueError('case not handled yet')
+        elif kwargs:
+            return make_dynamic_class(cls, Panther)
+        raise ValueError('Arguments are required to make a new class')
 
     def __init__(self, *args, **kwargs):
         self.kwargs = kwargs
         not_enum = Enum not in self.__class__.__bases__
         if not_enum:
-            if len(args):
-                self.value = args[0]
             if not issubclass(self.__class__, inheritable_primitive_types):
                 super().__init__(*args, **kwargs)
 
-# Cat (object type model)
+    _nullable = True
+
+# Panther (object type model)
 a = ComposedSchema(color='black')
-for cls in [ComposedSchema, Cat]:
+for cls in [ComposedSchema, Panther]:
     assert issubclass(a.__class__, cls)
     assert isinstance(a, cls)
 assert a.color == "black"
@@ -209,6 +226,79 @@ bases = (ComposedSchema, ListModel)
 assert a.__class__.__bases__ == bases
 assert a.value == value
 assert a == value
+
+
+class Animal:
+    def __new__(cls, *args, **kwargs):
+        return get_new_instance(Animal, cls, super(), *args, **kwargs)
+
+    @classmethod
+    def _get_new_class(cls, *args, **kwargs):
+        return make_dynamic_class(cls, Cat)
+
+    def __init__(self, *args, **kwargs):
+        self.kwargs = kwargs
+        not_enum = Enum not in self.__class__.__bases__
+        if not_enum:
+            if not issubclass(self.__class__, inheritable_primitive_types):
+                super().__init__(*args, **kwargs)
+
+
+class Cat:
+    def __new__(cls, *args, **kwargs):
+        return get_new_instance(Cat, cls, super(), *args, **kwargs)
+
+    @classmethod
+    def _get_new_class(cls, *args, **kwargs):
+        return make_dynamic_class(cls, Animal)
+
+    def __init__(self, *args, **kwargs):
+        self.name = kwargs.get('name')
+
+        self.kwargs = kwargs
+        not_enum = Enum not in self.__class__.__bases__
+        if not_enum:
+            if not issubclass(self.__class__, inheritable_primitive_types):
+                super().__init__(*args, **kwargs)
+
+
+# TypeError: Cannot create a consistent method resolution
+# order (MRO) for bases Animal, Cat
+# class Cat(Animal):
+#     def __init__(self, *args, **kwargs):
+#         self.name = kwargs.get('name')
+# animal_cat = Animal(name='Sprinkles')
+# pdb.set_trace()
+# pass
+
+
+# # Difficult example
+# Apple:
+#     properties:
+#     - apple_type
+#     type: string
+#
+# Pear:
+#     properties:
+#     - pear_type
+#     type: string
+#
+# Fruit:
+# oneOf
+# - Apple
+# - Pear
+#
+# Food:
+# allOf:
+# - Fruit
+#
+# # for food we assume that our class would look like
+# Food(Fruit)
+# # but if we did that, then Food would not be including the required Apple or Pear
+# we need (Fruit, Apple), which we get from running the __new__
+# if get back DynamicBaseClasses then we need to grab all of those bases
+
+# Qustion to self: can allOf clases be put in models as base classes automatically?
 
 # TODO a composed schema that includes an eum
 
